cddcb2edb8ff33f07d3e7064f23dacc4a2349149
Andrii Balan <balan.andrey.93@gmail.com>
2024-05-05 13:09:56 +0300

    dev: footer fix

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,19 +5,20 @@ const Footer = () => {
     return (
         <footer className="py-5 sm:px-10 px-5">
             <div className="screen-max-width">
-                <div className="">
+                <div>
                     <p className="font-semibold text-gray text-xs">
-                        More ways to shop: {"  "}
-                        <span className="underline text-blue"> Find an Apple store {"  "}</span>
-                        or <span className="underline text-blue"> other retailer {"  "}</span>
-                        near you.
+                        More ways to shop:{" "}
+                        <span className="underline text-blue">Find an Apple Store </span>
+                        or <span className="underline text-blue">other retailer</span> near you.
                     </p>
-                    <p className="font-semibold text-gray text-xs">Or call 000111-222-3456</p>
+                    <p className="font-semibold text-gray text-xs">Or call 000800-040-1966</p>
                 </div>
+
                 <div className="bg-neutral-700 my-5 h-[1px] w-full" />
-                <div className="flex md:glex-row flex-col md:items-center justify-between ">
+
+                <div className="flex md:flex-row flex-col md:items-center justify-between">
                     <p className="font-semibold text-gray text-xs">
-                        Copyright @2024 Apple Inc. All rights reserved.
+                        Copright @ 2024 Apple Inc. All rights reserved.
                     </p>
                     <div className="flex">
                         {footerLinks.map((link, i) => (
